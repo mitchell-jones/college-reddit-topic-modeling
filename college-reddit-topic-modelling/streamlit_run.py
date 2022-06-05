7fe6b52b2c0cc73617dc1384a843a57566f0df39
mitchell-jones <46410287+mitchell-jones@users.noreply.github.com>
2022-06-05 15:57:41 -0400

    Update streamlit_run.py

--- college-reddit-topic-modelling/streamlit_run.py
+++ college-reddit-topic-modelling/streamlit_run.py
@@ -9,6 +9,6 @@ import os
 import pandas as pd
 
 st.title('Using BERTopic for Topic Exporation of Student Discussion on Colleges in the State of North Carolina')
-df = pd.read_csv('https://github.com/mitchell-jones/college-reddit-topic-modeling/blob/main/college-reddit-topic-modelling/data/processed/NCSU%20Reddit%20Posts.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/mitchell-jones/college-reddit-topic-modeling/main/college-reddit-topic-modelling/data/processed/NCSU%20Reddit%20Posts.csv')
 
 st.write(df)
