484c233593709e46641a22ff1a7ef270312e9e74
mitchell-jones <46410287+mitchell-jones@users.noreply.github.com>
2022-06-05 15:51:35 -0400

    Update streamlit_run.py

--- college-reddit-topic-modelling/streamlit_run.py
+++ college-reddit-topic-modelling/streamlit_run.py
@@ -10,18 +10,4 @@ import pandas as pd
 
 st.title('Using BERTopic for Topic Exporation of Student Discussion on Colleges in the State of North Carolina')
 
-master_path = os.getcwd()
-
-selected_college = st.selectbox('Select a college for analysis',
-                                ['NCSU', 'UNCC', 'UNC', 'Duke'])
-
-if selected_college == 'NCSU':
-    path = master_path + r'/data/processed/NCSU Reddit Posts.csv'
-    df = pd.read_csv(path)
-    
-if selected_college == 'UNCC':
-    path = master_path + r'/data/processed/UNCC Reddit Posts.csv'
-    df = pd.read_csv(path)
-    
-
-st.write(df)
+st.write(os.listdir())
